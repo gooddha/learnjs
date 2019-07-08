e5e35707f22acc323d4fa505c3fca571103259a2
Andrey Adamovich <gooddha@ya.ru>
2019-07-08 17:13:19 +0300

    решаю задачу по таймерам

--- setTimer.js
+++ setTimer.js
@@ -18,7 +18,7 @@ printNumbersInterval();
 
 
 // https://learn.javascript.ru/settimeout-setinterval#vyvod-chisel-kazhdye-100-ms-cherez-settimeout
-
+/*
 function printNumbersRecursiveTimer() {
     let i = 0;
     function timer() {
@@ -35,3 +35,28 @@ function printNumbersRecursiveTimer() {
 }
 
 printNumbersRecursiveTimer();
+*/
+
+// https://learn.javascript.ru/settimeout-setinterval#funktsiya-zaderzhka
+
+function delay(f, ms) {
+    
+    return function() { 
+        let savedThis = this;
+        let savedArgs = arguments;
+
+        setTimeout(function() {
+            f.apply(savedThis, savedArgs)
+        }, ms);
+    };
+}
+
+function f(x) {
+    console.log(x);
+}
+
+var f1000 = delay(f, 1000);
+var f1500 = delay(f, 1500);
+
+f1000("тест"); // выведет "тест" через 1000 миллисекунд
+f1500("тест2"); // выведет "тест2" через 1500 миллисекунд
