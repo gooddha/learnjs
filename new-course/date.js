659ba3dbaf78fbd2bed0c75c203d59ec7535fd4b
Andrey Adamovich <gooddha@ya.ru>
2019-09-24 16:09:20 +0300

    done last task of date-time lesson

--- new-course/date.js
+++ new-course/date.js
@@ -118,9 +118,24 @@ alert(getSecondsToTomorrow());
 */
 
 function formatDate(date) {
-    switch(date) {
-        case 
+    let diff = Date.now() - date;
+
+    if (diff < 1000 && diff >= 0) {
+        return "прямо сейчас";
+    } else if (diff < 60 * 1000 && diff >= 0) {
+        return `${Math.round(diff / 1000)} сек. назад`;
+    } else if (diff < 60 * 60 * 1000 && diff >= 0) {
+        return `${Math.round(diff / 1000 / 60)} мин. назад`;
+    } else {
+        let day = date.getDate();
+        let month = (date.getMonth() + 1) < 10 ? "0" + (date.getMonth() + 1) : date.getMonth() + 1;
+        let year = date.getFullYear().toString().slice(2);
+        let hours = date.getHours();
+        let minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+
+        return `${ day }.${ month }.${ year } ${ hours }:${ minutes }`;
     }
+
 }
 
 alert( formatDate(new Date(new Date - 1)) ); // "прямо сейчас"
@@ -130,4 +145,4 @@ alert( formatDate(new Date(new Date - 30 * 1000)) ); // "30 сек. назад"
 alert( formatDate(new Date(new Date - 5 * 60 * 1000)) ); // "5 мин. назад"
 
 // вчерашняя дата вроде 31.12.2016, 20:00
-alert( formatDate(new Date(new Date - 86400 * 1000)) );
+alert( formatDate(new Date(+new Date + 86400 * 1000)) );
