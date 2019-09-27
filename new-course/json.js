885628768c50b0616177290c8fb5d7712387c0db
gooddha <gooddha@ya.ru>
2019-09-27 15:52:54 +0300

    done json lesson tasks

---
+++ new-course/json.js
@@ -0,0 +1,49 @@
+let alert = console.log;
+/*http://learn.javascript.ru/json#preobrazuyte-obekt-v-json-a-zatem-obratno-v-obychnyy-obekt
+Преобразуйте объект в JSON, а затем обратно в обычный объект
+Преобразуйте user в JSON, затем прочитайте этот JSON в другую переменную.
+
+let user = {
+  name: "Василий Иванович",
+  age: 35
+};
+
+let userJson = JSON.stringify(user);
+alert(userJson);
+let user2 = JSON.parse(userJson);
+alert(user2);
+*/
+
+/*http://learn.javascript.ru/json#isklyuchit-obratnye-ssylki
+Исключить обратные ссылки
+В простых случаях циклических ссылок мы можем исключить свойство, из-за которого они возникают, из сериализации по его имени.
+Но иногда мы не можем использовать имя, так как могут быть и другие, нужные, свойства с этим именем во вложенных объектах. Поэтому можно проверять свойство по значению.
+Напишите функцию replacer для JSON-преобразования, которая удалит свойства, ссылающиеся на meetup:
+*/
+
+let room = {
+  number: 23
+};
+
+let meetup = {
+  title: "Совещание",
+  occupiedBy: [{name: "Иванов"}, {name: "Петров"}],
+  place: room
+};
+
+// цикличные ссылки
+room.occupiedBy = meetup;
+meetup.self = meetup;
+
+alert( JSON.stringify(meetup, function replacer(key, value) {
+    return (key != '' && value == meetup) ? undefined : value;
+}));
+
+/* в результате должно быть:
+{
+  "title":"Совещание",
+  "occupiedBy":[{"name":"Иванов"},{"name":"Петров"}],
+  "place":{"number":23}
+}
+
+*/
